66634ae7daa162dc815e956c348db205939c26b5
Matheus Pereira <49734641+mathzpereira@users.noreply.github.com>
2023-06-07 10:53:30 -0300

    Delete src/main directory

--- src/main/java/module-info.java
+++
@@ -1,9 +0,0 @@
-module com.example.projetoelectrozone {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.sql;
-
-
-    opens com.example.projetoelectrozone to javafx.fxml;
-    exports com.example.projetoelectrozone;
-}
